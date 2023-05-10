8d70c996121976c1aa7026c1416ce86824213a61
kthomasstone <22228480+kthomasstone@users.noreply.github.com>
2023-05-10 18:53:11 -0500

    solve 01_helloWorld and 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,23 @@
-const repeatString = function() {
+const number = Math.floor(Math.random() * 1000);
+const repeatString = function (theString, num) {
+  let totalString = "";
 
+  for (let i = 0; i < num; i++) {
+    totalString += theString;
+  }
+  if (num < 0) {
+    totalString = "ERROR";
+  }
+
+  return totalString;
 };
+repeatString("hey", 3);
+repeatString("hey", 10);
+repeatString("hey", 1);
+repeatString("hey", 0);
+repeatString("hey", -1);
+repeatString("hey", number);
+repeatString("hey", "");
 
 // Do not edit below this line
 module.exports = repeatString;
