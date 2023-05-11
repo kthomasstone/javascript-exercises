0f46cbd584e72442180410e5750a92495551c774
kthomasstone <22228480+kthomasstone@users.noreply.github.com>
2023-05-10 21:27:52 -0500

    solve 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
-
+const reverseString = function (stringToReverse) {
+  // store new string in a variable
+  let newString;
+  // split the stringToReverse into an array, reverse the items, then join them back into a string
+  newString = stringToReverse.split("").reverse().join("");
+  // return the new string
+  return newString;
 };
+
+reverseString("hello");
+reverseString("hello there");
+reverseString("123! abc!");
+reverseString("");
 
 // Do not edit below this line
 module.exports = reverseString;
