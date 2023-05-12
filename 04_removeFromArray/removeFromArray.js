550a5a80fd2c26bbd5983281b068c04c59438cd3
kthomasstone <22228480+kthomasstone@users.noreply.github.com>
2023-05-11 22:01:20 -0500

    solve 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function (theArray, ...itemToRemove) {
+  let newArray = [];
 
+  theArray.filter((element) => {
+    if (!itemToRemove.includes(element)) {
+      newArray.push(element);
+    }
+  });
+
+  console.log(newArray);
+  return newArray;
 };
+
+removeFromArray([1, 2, 3, 4], 3);
+removeFromArray([1, 2, 3, 4], 3, 2);
+removeFromArray([1, 2, 3, 4], 7, "tacos");
+removeFromArray([1, 2, 3, 4], 7, 2);
+removeFromArray([1, 2, 3, 4], 1, 2, 3, 4);
+removeFromArray(["hey", 2, 3, "ho"], "hey", 3);
+removeFromArray([1, 2, 3], "1", 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
