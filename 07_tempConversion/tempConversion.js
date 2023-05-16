b5365b55be1c7ada3b8305a6094c739e3d5ecc5a
kthomasstone <22228480+kthomasstone@users.noreply.github.com>
2023-05-15 21:30:30 -0500

    solve 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,31 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrTemp) {
+  let celTemp;
+  // °C = (°F - 32) * 5/9
+  celTemp = ((fahrTemp - 32) * (5 / 9)).toFixed(1);
+  celTemp = Number(celTemp);
+  console.log(celTemp);
+  return celTemp;
 };
 
-const convertToFahrenheit = function() {
+convertToCelsius(32);
+convertToCelsius(100);
+convertToCelsius(-100);
+
+const convertToFahrenheit = function (celTemp) {
+  let fahrTemp;
+  // °F = (°C × 9/5) + 32
+  fahrTemp = (celTemp * (9 / 5) + 32).toFixed(1);
+  fahrTemp = Number(fahrTemp);
+  console.log(fahrTemp);
+  return fahrTemp;
 };
+
+convertToFahrenheit(0);
+convertToFahrenheit(73.2);
+convertToFahrenheit(-10);
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
