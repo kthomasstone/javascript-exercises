4749f291bec56fc9cd4a57559461fc5bc8236b62
kthomasstone <22228480+kthomasstone@users.noreply.github.com>
2023-09-15 21:41:34 -0500

    complete all calculator exercises.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((total, nextNum) => total + nextNum, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.length ? array.reduce((total, nextNum) => total * nextNum) : 0;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+// multiply
+
+const factorial = function(num) {
+  let total = 1;
+  for (let i = num; i > 0; i--) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
@@ -29,5 +35,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
