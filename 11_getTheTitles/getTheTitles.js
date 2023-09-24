92317057664016583536755402b29b5ce3678590
kthomasstone <22228480+kthomasstone@users.noreply.github.com>
2023-09-24 14:35:56 -0500

    complete puzzle 11.

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,21 @@
 const getTheTitles = function() {
+  const books = [
+    {
+      name: "Book",
+      author: "Name",
+    },
+    {
+      name: "Book2",
+      author: "Name2",
+    },
+  ];
 
+  let arrayOfNames = [];
+
+  for (let i = 0; i < books.length; i++) {
+    arrayOfNames.push(books[i].name);
+  }
+  return arrayOfNames;
 };
 
 // Do not edit below this line
