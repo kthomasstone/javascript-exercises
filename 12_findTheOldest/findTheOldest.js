f3c1114fe3365d951f97732ef13c1a3f0b6ada8e
kthomasstone <22228480+kthomasstone@users.noreply.github.com>
2023-09-24 18:48:47 -0500

    complete puzzle 12.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,35 @@
 const findTheOldest = function() {
+  const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ];
 
+  const getAge = function(birth, death) {
+    if (!death) {
+      death = new Date().getFullYear();
+    }
+    return death - birth;
+  };
+
+  for (i = 0; i < people.length; i++) {
+    people[i].age = getAge(people[i].yearOfBirth, people[i].yearOfDeath);
+  }
+
+  people.sort((person1, person2) => person2.age - person1.age);
+  return people[0];
 };
 
 // Do not edit below this line
